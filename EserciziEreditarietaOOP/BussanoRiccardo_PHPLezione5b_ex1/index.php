e491a7622b3b351def8b6765e2f30f3e22c5ba56
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2024-10-31 03:21:59 +0100

    feat: OOP Inheritance exercise 1

---
+++ EserciziEreditarietaOOP/BussanoRiccardo_PHPLezione5b_ex1/index.php
@@ -0,0 +1,115 @@
+<?php
+
+declare(strict_types=1);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        @import url("https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap");
+
+        * {
+            font-family: "Inter", sans-serif;
+        }
+    </style>
+
+    <title>Veicoli</title>
+</head>
+
+<body class="bg-zinc-950 text-neutral-50">
+    <?php
+    class Veicolo
+    {
+        private string $marca;
+
+        public function __construct(string $marca)
+        {
+            $this->setMarca($marca);
+        }
+
+        public function getMarca(): string
+        {
+            return $this->marca;
+        }
+
+        public function setMarca(string $marca): void
+        {
+            if (!is_string($marca)) {
+                throw new Exception("Marca non valida");
+            }
+            if (empty($marca)) {
+                throw new Exception("Marca non valida");
+            }
+
+            $this->marca = $marca;
+        }
+
+        // Metodi richisti
+        public function accellera(): string
+        {
+            return "Veicolo in accelerazione " . $this->getMarca();
+        }
+    }
+
+    final class Automobile extends Veicolo
+    {
+        public function accellera(): string
+        {
+            return "Automobile in accelerazione " . $this->getMarca();
+        }
+    }
+
+    final class Moto extends Veicolo
+    {
+        public function accellera(): string
+        {
+            return "Moto in accelerazione " . $this->getMarca();
+        }
+    }
+
+    function validate_data(string $data): string
+    {
+        $data = trim($data);
+        $data = htmlspecialchars($data);
+        $data = stripslashes($data);
+        return $data;
+    }
+
+    $veicolo = new Veicolo("Goofy Car");
+    $automobile = new Automobile("BMW");
+    $moto = new Moto("Piagio");
+    ?>
+    <div class="w-full h-screen flex flex-col justify-center items-center gap-4">
+        <form class="flex gap-4" method="get" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
+            <button type="submit" name="type" value="veicolo" class="px-4 py-2 bg-neutral-50 text-zinc-950 rounded-md">Accelera veicolo</button>
+            <button type="submit" name="type" value="automobile" class=" px-4 py-2 bg-neutral-50 text-zinc-950 rounded-md">Acccellera automobile</button>
+            <button type="submit" name="type" value="moto" class=" px-4 py-2 bg-neutral-50 text-zinc-950 rounded-md">Acccelera Moto</button>
+        </form>
+        <div>
+            <?php
+            if ($_SERVER["REQUEST_METHOD"] == "GET") {
+                $type = validate_data($_GET["type"]);
+                if (empty($type)) {
+                    throw new Exception("Insert a valid type");
+                }
+
+                if ($type == "veicolo") {
+                    echo $veicolo->accellera();
+                } else if ($type == "automobile") {
+                    echo $automobile->accellera();
+                } else if ($type == "moto") {
+                    echo $moto->accellera();
+                } else {
+                    throw new Exception("Insert a type that exist");
+                }
+            }
+            ?>
+        </div>
+    </div>
+</body>
+
+</html>
