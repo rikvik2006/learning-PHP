0060aaf0ffe593f18f336020c7cd77f8403d5d34
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2024-12-05 13:49:32 +0100

    feat: add basic frontend style

---
+++ Calcolatrice/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="styles/app.css">
+    <title>Calcolatrice</title>
+</head>
+
+<body>
+    <div class="calculator_container">
+        <div class="screen_container">
+            <input type="text" class="screen" value="0">
+        </div>
+        <div class="keyboard_container">
+            <button class="btn">1</button>
+            <button class="btn">2</button>
+            <button class="btn">3</button>
+            <button class="btn">+</button>
+            <button class="btn">4</button>
+            <button class="btn">5</button>
+            <button class="btn">6</button>
+            <button class="btn">-</button>
+            <button class="btn">7</button>
+            <button class="btn">8</button>
+            <button class="btn">9</button>
+            <button class="btn">/</button>
+            <button class="btn">0</button>
+            <button class="btn">C</button>
+            <button class="btn">=</button>
+            <button class="btn">*</button>
+        </div>
+    </div>
+</body>
+
+</html>
