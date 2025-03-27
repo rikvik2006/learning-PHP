9a4fb7f5272171baf7444085f915e6a5086e006e
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2025-03-27 13:41:32 +0100

    feat: creating the main page

---
+++ Jukebox/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/main.css">
+
+    <title>Home Jukebox</title>
+</head>
+
+<body>
+    <div class="main">
+        <section class="content_section">
+            <div class="header_container">
+                <div class="links_container">
+                    <div>Home</div>
+                    <div>Jukebox Manager</div>
+                </div>
+                <div class="searchbar">Cerca</div>
+            </div>
+            <section class="song_selection_section">
+                <div class="recently_listened">
+                    <h1>Ascoltati di recente</h1>
+                </div>
+            </section>
+        </section>
+        <section class="player_section">
+            <div>Player</div>
+        </section>
+    </div>
+</body>
+
+</html>
