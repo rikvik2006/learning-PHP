4d9d0820e27f34b1da4066a052b97c943e155786
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2025-04-22 22:15:51 +0200

    feat: create navbar

--- Jukebox/index.php
+++ Jukebox/index.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles/main.css">
+    <link rel="stylesheet" href="./styles/home/home.css">
+    <link rel="stylesheet" href="./styles/global/navbar.css">
 
     <title>Home Jukebox</title>
 </head>
@@ -17,15 +19,30 @@
                     <div>Home</div>
                     <div>Jukebox Manager</div>
                 </div>
-                <div class="searchbar">Cerca</div>
+                <form class="searchbar" action="" method="get">
+                    <img src="./img/magnifing_glass.svg" alt="Search Icon">
+                    <input type="text" placeholder="Cerca">
+                </form>
             </div>
             <section class="song_selection_section">
                 <div class="recently_listened">
-                    <h1>Ascoltati di recente</h1>
-                    <div></div>
+                    <h1 class="section_title">Ascoltati di recente</h1>
+                    <!-- End Song -->
+                    <div class="song_item">
+                        <div class="song_image">
+                            <img src="./img/album_cover.jpg" alt="Album Cover">
+                        </div>
+                        <div class="song_details">
+                            <div class="song_type">Brano</div>
+                            <h2>Song Title</h2>
+                            <p>Artist Name</p>
+                        </div>
+                    </div>
+                    <!-- End Song -->
+                    <div>Ciao</div>
                 </div>
             </section>
-        </section>
+        </section>1
         <section class="player_section">
             <div>Player</div>
         </section>
