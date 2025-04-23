31a6d4552cfa22a414159eec9a389cff438a68d2
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2025-04-23 21:55:30 +0200

    feat: create song home page template

--- Jukebox/index.php
+++ Jukebox/index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="./styles/main.css">
     <link rel="stylesheet" href="./styles/home/home.css">
     <link rel="stylesheet" href="./styles/global/navbar.css">
+    <link rel="stylesheet" href="./styles/global/song.css">
 
     <title>Home Jukebox</title>
 </head>
@@ -30,12 +31,14 @@
                     <!-- End Song -->
                     <div class="song_item">
                         <div class="song_image">
-                            <img src="./img/album_cover.jpg" alt="Album Cover">
+                            <img src="./img/gnx album cover.jpg" alt="Album Cover">
                         </div>
                         <div class="song_details">
                             <div class="song_type">Brano</div>
-                            <h2>Song Title</h2>
-                            <p>Artist Name</p>
+                            <div class="title_artist_container">
+                                <div class="song_artist">Kendrick Lamar</div>
+                                <h2 class="song_title">Song tv off (feat. lefty gunplay)</h2>
+                            </div>
                         </div>
                     </div>
                     <!-- End Song -->
---
+++ Jukebox/test/song.php
@@ -0,0 +1,14 @@
+<!-- End Song -->
+<div class="song_item">
+    <div class="song_image">
+        <img src="./img/gnx album cover.jpg" alt="Album Cover">
+    </div>
+    <div class="song_details">
+        <div class="song_type">Brano</div>
+        <div class="title_artist_container">
+            <div class="song_artist">Kendrick Lamar</div>
+            <h2 class="song_title">Song tv off (feat. lefty gunplay)</h2>
+        </div>
+    </div>
+</div>
+<!-- End Song -->
